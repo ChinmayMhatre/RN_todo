977ad3579572be3016ab20414cdbb50ee3c773fd
chinmay <chinmaymhatre12@gmail.com>
2022-05-13 23:17:54 +0530

    bugs  fixed

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider } from "native-base";
+import Home from "./comps/Home.js";
+import Edit from "./comps/Edit.js";
+import New from "./comps/New.js";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const Stack = createNativeStackNavigator();
+    LogBox.ignoreAllLogs();
+    return (
+        <NativeBaseProvider>
+            <NavigationContainer>
+                <Stack.Navigator
+                    initialRouteName="Home"
+                    screenOptions={{
+                        title: "Todo List",
+                        headerTitleAlign: "center",
+                        headerStyle: {
+                            backgroundColor: "#141E30",
+                        },
+                        headerTintColor: "#fff",
+                    }}
+                >
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="Edit" component={Edit} />
+                    <Stack.Screen name="New" component={New} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
